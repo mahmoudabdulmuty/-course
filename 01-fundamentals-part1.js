4ce0dcb54ad0e90d36687084ee760be89955ae65
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-26 20:39:13 +0200

    Data types

--- 01-fundamentals-part1.js
+++ 01-fundamentals-part1.js
@@ -6,3 +6,15 @@ let population = 100;
 console.log(country, continent, population);
 
 /*****************************************************/
+
+// Data Types
+const isIsland = false;
+let language;
+console.log(
+	typeof isIsland,
+	typeof population,
+	typeof country,
+	typeof language
+);
+
+/*****************************************************/
