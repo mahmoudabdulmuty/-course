4d57cc6fdcb19bf846e2d475b3484c7a52de1a64
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-26 20:38:09 +0200

    adding Values and Variables

---
+++ 01-fundamentals-part1.js
@@ -0,0 +1,8 @@
+'user strict';
+// Values and Variables
+const country = 'Egypt',
+	continent = 'Africa';
+let population = 100;
+console.log(country, continent, population);
+
+/*****************************************************/
