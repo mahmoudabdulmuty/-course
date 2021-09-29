7b324c26e5f5cec4ee66de5f7e9fe2ebc0a656c0
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-29 03:05:23 +0200

    Functions

---
+++ 02-fundamentals-part2.js
@@ -0,0 +1,9 @@
+'use strict';
+// Functions
+const describeCountry = function (country, population, capitalCity) {
+  console.log(`${country} has ${population} million people and its capital city is ${capitalCity}`);
+}
+describeCountry('Egypt', 100, 'Cairo')
+describeCountry('Finland', 6, 'Helsinki')
+describeCountry('Germany', 80, 'Berlin')
+
