90b7d73179446802fe99c7443f7ea49cd1f34e35
mahmoud <mahmoudabdulmuty@gmail.com>
2021-09-29 12:30:15 +0200

    Function Declarations vs. Expressions

--- 02-fundamentals-part2.js
+++ 02-fundamentals-part2.js
@@ -5,5 +5,21 @@ const describeCountry = function (country, population, capitalCity) {
 }
 describeCountry('Egypt', 100, 'Cairo')
 describeCountry('Finland', 6, 'Helsinki')
-describeCountry('Germany', 80, 'Berlin')
+describeCountry('Germany', 83, 'Berlin')
+describeCountry('Portugal', 10, 'Lisbon')
+
+/*****************************************************/
+
+// Function Declarations vs. Expressions
+function percentageOfWorld1(population) {
+  return ((population / 7900) * 100).toFixed(1)
+}
+const ChinaPercentage = percentageOfWorld1(1441)
+const EgyptPercentage = percentageOfWorld1(100)
+const GermanyPercentage = percentageOfWorld1(83)
+const FinlandPercentage = percentageOfWorld1(6)
+const PortugalPercentage = percentageOfWorld1(10)
+console.log(ChinaPercentage, EgyptPercentage, GermanyPercentage, FinlandPercentage, PortugalPercentage);
+
+/*****************************************************/
 
