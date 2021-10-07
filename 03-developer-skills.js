f695ae238066dafd16853d1836fe0e8fcc09e254
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-10-07 03:26:25 +0200

    Challenge#1

--- 03-developer-skills.js
+++ 03-developer-skills.js
@@ -25,6 +25,8 @@ const amplitude = arr => {
 const amplitudeVal = amplitude(temps);
 console.log(amplitudeVal);
 
+/*****************************************************/
+
 // problem#2 : finding the amplitude of 2 arrays
 
 const t1 = [1, -1, -2, 4, 19, 5, 10, 'err', 32, 3];
@@ -55,3 +57,27 @@ const amplitudeOfTwoArrays = (arr1, arr2) => {
 
 const amplitudeVal2 = amplitudeOfTwoArrays(t1, t2);
 console.log(amplitudeVal2);
+
+/*****************************************************/
+
+// Challenge#1
+
+// print forcast temp like this :
+// '... 17c in 1 days ... 21c in 2 days ... 23c in 3 days ...'
+
+const printForcast = arr => {
+	let statement = ``;
+	for (let i = 0; i < arr.length; i++) {
+		let temp = arr[i];
+		let day = i + 1;
+		statement += `... ${temp}c in ${day} ${day === 1 ? `day` : `days`} `;
+	}
+	statement += ` ...`;
+	return statement;
+};
+
+const arr1 = [17, 21, 23]; // TEST DATA 1
+const arr2 = [12, 5, -5, 0, 4]; // TEST DATA 2
+
+console.log(printForcast(arr1));
+console.log(printForcast(arr2));
